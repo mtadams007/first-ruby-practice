71fa5518dc09346b5569a3b7bbcde7e2b298af50
Michael Adams <mtadams007@gmail.com>
2018-04-04 15:18:59 -0400

    Looping through an array assignment

--- practice.rb
+++ practice.rb
@@ -21,15 +21,51 @@
 # If the number is divisible by both five and three print out "Fizz Buzz"
 # If the number is not divisible by either five or three print out "(num) is not fizzable"
 
-puts "Give me a number!"
-number = gets.chomp
-num = number.to_i
-if num % 3 == 0 && num % 5 == 0
-  puts "Buzz Fizz"
-elsif num % 3 == 0
-  puts "Buzz"
-elsif num % 5 == 0
-  puts "Fizz"
-else
-  puts "#{num} is not fizzable :("
+# puts "Give me a number!"
+# num = gets.chomp.to_i
+# if num % 3 == 0 && num % 5 == 0
+#   puts "Buzz Fizz"
+# elsif num % 3 == 0
+#   puts "Buzz"
+# elsif num % 5 == 0
+#   puts "Fizz"
+# else
+#   puts "#{num} is not fizzable :("
+# end
+
+# Write a command line app that takes in a variable with arrays nested as values inside another array. Print out sentences for each of them.
+# people = [["Jason", "Maria", "Justin"], ["Red", "Blue", "Green"], ["Limp Bizkit", "3 Doors Down", "Papa Roach"]]
+#
+# The sentence will look like "Jason's favorite color is Red, and he loves Limp Bizkit"
+
+people = [["Jason", "Maria", "Justin"], ["red", "blue", "green"], ["Limp Bizkit", "3 Doors Down", "Papa Roach"]]
+
+statement = ''
+statement2 = ''
+statement3 = ''
+people.each_index do |i|
+  if i == 0
+    statement += "#{people[i][0]}'s favorite color is "
+    statement2 += "#{people[i][1]}'s favorite color is "
+    statement3 += "#{people[i][2]}'s favorite color is "
+  elsif i == 1
+    statement += "#{people[i][0]}, and he loves "
+    statement2 += "#{people[i][1]}, and she loves "
+    statement3 += "#{people[i][2]}, and he loves "
+  elsif i == 2
+    statement += "#{people[i][0]}"
+    statement2 += "#{people[i][1]}"
+    statement3 += "#{people[i][2]}"
+    puts statement
+    puts statement2
+    puts statement3
+  end
+end
+
+#More elegant solution stolen from Andrew
+
+i = 0
+until i == people.length do
+	puts ("#{people[0][i]}'s favorite color is #{people[1][i]} and they love #{people[2][i]}")
+	i += 1
 end
